e78d8d11b4a6d72670c812db1064f75fb7411377
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-01-14 18:47:50 +0530

    map

--- src/components/News.js
+++ src/components/News.js
@@ -108,17 +108,15 @@ export class News extends Component {
       <div className='container my-3'>
       <h2>NewsMonky - Top Headlines</h2> 
       <div className='row'>
-      <div className='col-md-4'>
-        <Newsitem title="myTitle" description='mydesc'
-              imageUrl="https://cdn.vox-cdn.com/thumbor/8k7JPu5PSipDoLQ5SJhtTvOxiYQ=/0x0:2040x1362/1200x628/filters:focal(1020x681:1021x682)/cdn.vox-cdn.com/uploads/chorus_asset/file/25825968/Installer_66.png" 
-              newsUrl="todo"/>
-     </div>
-          <div className='col-md-4'>
-            <Newsitem title="myTitle" description='mydesc' /></div>
-          <div className='col-md-4'>
-            <Newsitem title="myTitle" description='mydesc' /></div>         
-      </div>
-      </div>
+      {this.state.articles.map((element)=>{
+        return <div className='col-md-4'>
+          <Newsitem key={element.url}title="myTitle" description='mydesc'
+            imageUrl="https://cdn.vox-cdn.com/thumbor/8k7JPu5PSipDoLQ5SJhtTvOxiYQ=/0x0:2040x1362/1200x628/filters:focal(1020x681:1021x682)/cdn.vox-cdn.com/uploads/chorus_asset/file/25825968/Installer_66.png"
+            newsUrl="todo" />
+        </div>
+  })}
+   </div>
+    </div>
     )
   }
 }
