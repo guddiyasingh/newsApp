16fdfce409893d4a7607b0df9f2f5b7f3b897861
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-01-25 00:51:29 +0530

    add auther name, Date

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -3,14 +3,16 @@ import React, { Component } from 'react'
 export class Newsitem extends Component {
 
   render() {
-   let {title,description,imageUrl,newsUrl} = this.props;
+   let {title,description,imageUrl,newsUrl,author,date} = this.props;
     return (
       <div className='my-3'>
         <div className="card" >
-          <img src={imageUrl} class Name="card-img-top" alt="..."/>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
-              <h5 className="card-title">{title}</h5>
-              <p className="card-text">{description}</p>
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}</p>
+            <p className="card-text"><small className="text-body-secondary">By {!author?"unknown":author} on {new Date(date).toGMTString()} </small></p>
+
               <a rel="noreferrer" href={newsUrl}target='_blanck' className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
