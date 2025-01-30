ecf0ac6a6e8940538ebc3750535220828285d192
Guddiya Kumari <guddiyasingh6789@gmail.com>
2025-01-31 00:32:37 +0530

    remove z-index

--- src/components/Newsitem.js
+++ src/components/Newsitem.js
@@ -7,12 +7,14 @@ export class Newsitem extends Component {
     return (
       <div className='my-3'>
         <div className="card" >
-          <img src={imageUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-            <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger"
-            style={{left:'90',zIndex:"1"}}>
+          <div>
+            <span className="badge rounded-pill bg-danger"
+              >
               {source}
             </span>
+          </div>
+          <img src={imageUrl} className="card-img-top" alt="..."/>
+            <div className="card-body">
             <h5 className="card-title">{title}
             </h5>
             <p className="card-text">{description}</p>
